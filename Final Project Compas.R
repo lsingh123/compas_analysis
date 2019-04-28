edc6ef376f1f93118825049c9537148b7b0eb643
Lavanya Singh <lavanyasingh@Lavanyas-MacBook-Air-011219.local>
2019-04-28 15:40:49 -0400

    used ggplot instead of built-in functions, added gamma distribution for second chunk

--- Final Project Compas.R
+++ Final Project Compas.R
@@ -1,27 +1,37 @@
 compas <- read.csv ("compass/compas-scores-raw.csv"); head(compas)
+library(ggplot2)
 
-#messy
-hist(compas$RawScore, breaks = "FD", probability = TRUE)
+#messy but there are two clear clusters we can isolate
+ggplot(compas, aes(x=RawScore)) + geom_histogram()
 
-#first chunk -> scores < 7
-scores <- compas[which(compas$RawScore < 7),]; head(scores1)
+#first cluster -> scores < 7 (this is an estimate, 
+#should we try using a clustering algorithm like k-means or something?)
+scores <- compas[which(compas$RawScore < 7),]; head(scores)
 max <- max(scores$RawScore)
 min <- min(scores$RawScore)
 #squeezing the values to be between 0 and 1
-score <- (max - scores$RawScore) / (max - min)
-hist(score, breaks = "FD", probability = TRUE)
-mu <- mean(score); mu
-var <- var(score); var
+scores$score <- (max - scores$RawScore) / (max - min)
+plot <- ggplot(scores, aes(score)) + geom_histogram(aes(y = stat(density))); plot
+mu <- mean(scores$score); mu
+var <- var(scores$score); var
 #used Wolfram Alpha to find parameters for a beta distribution by solving a system of equations
 param1 <- 15162/3725
 param2 <- 11913/3725
-#dbeta looks accurate!
-curve(dbeta(x, param1, param2), add = TRUE, col = "red")
+#let's try dbeta
+stat <- stat_function(fun = dbeta, args = list(param1, param2), lwd = 1, col = "red")
+#that looks quite accurate!
+plot + stat
 
+#second cluster -> scores > 7
 scores2 <- compas[which(compas$RawScore >= 7),]
-hist(scores2$RawScore, breaks = "FD", probability = TRUE)
-
-compas2 <- read.csv("compass/cox-violent-parsed_filt.csv"); head(compas2)
-max(stroi(compas2$c_days_from_compas))
-compas3 <- read.csv("compass/cox-violent-parsed.csv"); head(compas3)
-compas4 <- read.csv("compass/propublica_data_for_fairml.csv"); head(compas4)
+#translating the data to the origin
+#and transforming it to lie between 0 and 10
+scores2$score <- (scores2$RawScore - min(scores2$RawScore)) / 4
+mu <- mean(scores2$score) ; mu
+var <- var(scores2$score); var
+plot <- ggplot(scores2, aes(score)) + geom_histogram(aes(y = stat(density)), binwidth = 0.25); plot
+#let's try dgamma
+stat <- stat_function(fun = dgamma, args = list(mu), lwd = 1, col = "red")
+#dgamma appears to fit!
+plot + stat
+#do our transformations make the distributions that we fit useless? 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # compas_analysis
+
+Math 23-C: Mathematics for Computation, Statistics, and Data Science Final Project
+
+## sources
+
+Data gathered from the ProPublica analysis of the COMPAS algorithm via kaggle. 
+
+Access link: https://www.kaggle.com/danofer/compass
